96ace3f720f2f1d06ecdbda0685f0cb338539ed7
dynamicscode <c.akhaing@gmail.com>
2019-12-30 10:13:10 +1300

    Email validator component

--- main.js
+++ main.js
@@ -1,31 +1,7 @@
 console.log('CustomElement');
 
-class CustomElement extends HTMLElement {
-    set value(v) {
-        this._value = v;
-    }
-
-    get value() {
-        return this._value;
-    }
-
-    updateValue(v) {
-        this._value = v;
-        this.linkedNode.setValue(this.value);
-        this.updateView();
-    }
-
-    set link(id) {
-        this.linkedNode = Xrm.Portal.Form.get(id);
-        this.linkedNode.c[0].setAttribute('style', 'display:none;');
-        this.linkedNode.c[0].onchange = () => { this.update(this.linkedNode.getValue()); }
-        this.updateValue(this.linkedNode.getValue());
-    }
-}
-
-
 // Create a class for the element
-class Increment extends CustomElement {
+class Increment extends HTMLInputElement {
     updateView() {
         this.textInput.value = this.value;
     }
@@ -74,18 +50,65 @@ class Increment extends CustomElement {
     }
 }
 
-function registerComponent(tagName, type) {
-    customElements.define(tagName, type);
-}
+class EmailValidator extends CustomElement {
+    updateView() {
+        this.textInput.value = this.value;
+    }
 
-function replaceControl(id) {
-    const pocCtrl = document.createElement('increment-control');
-    pocCtrl.link = id;
-    Xrm.Portal.Form.get(id).c[0].parentElement.append(pocCtrl);
-}
+    setErrorMessage(msg) {
+        this.message.textContent = msg;
+    }
 
-function showRootControl(id) {
-    document.getElementById(id).hidden = false;
+    validateEmail() {
+        var email = this.textInput.value;
+        if (email.length < 1) {
+            this.setErrorMessage('Please enter an email address');
+        } else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+            this.setErrorMessage('');
+            this.updateValue(email);
+        }
+        else  {
+            this.setErrorMessage('Please enter a valid email address.');
+        }        
+    }
+
+    constructor() {
+        // Always call super first in constructor
+        super();
+
+        // Create a shadow root
+        this.shadow = this.attachShadow({ mode: 'open' });
+
+        // Create spans
+        this.wrapper = document.createElement('span');
+        this.wrapper.setAttribute('class', 'wrapper');
+
+        this.textInput = document.createElement('input');
+        this.textInput.setAttribute('class', 'text form-control');
+        this.textInput.setAttribute('tabindex', 0);
+        this.textInput.setAttribute('value', this.value ?? '');
+        this.textInput.onchange = () => this.validateEmail();
+
+        this.message = document.createElement('span');
+
+        // Create some CSS to apply to the shadow dom
+        const style = document.createElement('style');
+        console.log(style.isConnected);
+
+        style.textContent = `
+        .wrapper {
+          position: relative;
+        }
+      `;
+
+        // Attach the created elements to the shadow dom
+        this.shadow.appendChild(style);
+        console.log(style.isConnected);
+        this.shadow.appendChild(this.wrapper);
+        this.wrapper.appendChild(this.textInput);
+        this.wrapper.appendChild(this.message);
+    }
 }
 
 registerComponent('increment-control', Increment);
+registerComponent('emailvalidator-control', EmailValidator);
